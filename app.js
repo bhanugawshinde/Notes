4ecc04301ec970d918a2e242ec1b29c08d12265d
Bhanu <bhanugawshinde@gmail.com>
2022-06-11 22:51:14 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,73 @@
+showNotes();
+let addbtn = document.getElementById("add");
+ addbtn.addEventListener("click",function(e){
+    let addTxt = document.getElementById("text");
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj=JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value="";
+    showNotes();
+});
+//Function to show elements from localstorage
+function showNotes(){
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj=JSON.parse(notes);
+    }
+    let html = "";
+    notesObj.forEach(function(element, index) {
+        html+=`  
+    
+        <div  class="noteCard setText">
+            <h1>Note${index+1}</h1>
+           <p id="note">${element}</p>
+            <button id="${index}" class ="delet" onclick="deletNote(this.id)" >delet</button>
+        </div>`;
+    });
+    let notesElm = document.getElementById("notes");
+    if(notesObj.length !=0){
+        notesElm.innerHTML=html;
+    }
+    else{
+        notesElm.innerHTML=`Nothing to show!`;
+    }
+}
+function deletNote(index){
+console.log("deleting node"+index);
+let notes = localStorage.getItem("notes");
+if(notes == null){
+    notesObj = [];
+}
+else{
+    notesObj=JSON.parse(notes);
+}
+notesObj.splice(index,1);
+localStorage.setItem("notes", JSON.stringify(notesObj));
+showNotes();
+
+};
+
+let search=document.getElementById("searchTxt");
+search.addEventListener("input" , function(){
+    let inputVal= search.value.toLowerCase();
+    // console.log('Input event fired!', inputVal);
+let noteCard= document.getElementsByClassName("noteCard");
+Array.from(noteCard).forEach(function(element){
+    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+if(cardTxt.includes(inputVal)){
+    element.style.display="block";
+}
+else{
+    element.style.display="none";
+}
+})
+});
